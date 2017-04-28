704ce8ebb6745d342ac88e25ac41da16856278db
Sean <sjgallen@gmail.com>
2017-04-28 17:56:21 -0500

    used some ES6 and added a port variable

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
+const port = 4000;
 
 app.use(express.static('./server/static/'));
 app.use(express.static('./client/dist/'));
 
-app.listen(4000, () => {
-  console.log('Look on http://localhost:4000');
+app.listen(port, () => {
+  console.log(`Look on http://localhost: ${port}`);
 });
